0891d71b5ac29348a01af5aa70beca3123b20a39
Karina Cabrera <kart.p14@gmail.com>
2023-03-07 12:05:59 -0400

    feature/RI-748: Updating the automated test to work with a user of type Caller

--- test/moduleCallerAdmin/callScheduledContact.spec.ts
+++ test/moduleCallerAdmin/callScheduledContact.spec.ts
@@ -7,42 +7,43 @@ if (envFound.error) {
 
 const env = process.env
 
-describe('Call the scheduled contact', () => {
+describe('Reschedule a callback to a Donor', () => {
   // Login
   const url = env.PFR_URL + 'auth/login'
 
   it('Login', async () => {
     await browser.url(url)
     await browser.pause(3000)
-    await $('#username').setValue(env.SITEADMIN_USERNAME)
-    await $('#password').setValue(env.SITEADMIN_PASSWORD)
+    await browser.maximizeWindow()
+    await $('#username').setValue(env.CALLER_USERNAME)
+    await $('#password').setValue(env.CALLER_PASSWORD)
     await $('input[type="submit"]').click()
+    await browser.pause(3000)
   })
 
-  // Call a scheduled contact
+  // Callback Requested
 
-  it('Call a scheduled contact', async () => {
-    //Nav
-    await $('.dropdown=Caller').click()
-    await browser.pause(1000)
-    await $('.dropdown').$('//nav/div/div[2]/ul[2]/li[3]/ul/li[3]').click()
+  it('Callback Requested', async () => {
+    // Nav
+    await $('//nav/div/div[2]/ul[2]/li[3]/a').click()
+    await expect($('h1=Scheduled Calls')).toBeExisting()
     await browser.pause(2000)
-
+    // Call
     const companyContactTable = await $('//table/tbody[1]/tr/td[1]')
-    const nameContactTable = await $('//table/tbody[1]/tr/td[2]')
-    const btnCall = await $('//table/tbody[1]/tr/td[9]/a')
-    const companyContText = companyContactTable.getText()
-    const nameContText = nameContactTable.getText()
+    const nameContactTable = await $('.donorName')
+    const companyContText = await companyContactTable.getText()
+    const nameContText = await nameContactTable.getText()
 
-    $(btnCall).click()
-
-    const companyCall = await $('.nameAndCompany .company').getText()
-    const nameCall = await $('.donorName').getText()
-
-    await expect($('h1=Call')).toBeExisting()
-    expect(companyContText).toHaveTextContaining(companyCall)
-    expect(nameContText).toHaveText(nameCall)
+    await browser.pause(3000)
+    await $('.btn-primary').click()
     await browser.pause(3000)
 
+    const companyCall = await $('//div[@id="donorView"]/div[1]/div[2]')
+    const nameCall = await $('.donorName')
+
+    await expect($('h1=Call')).toBeExisting()
+    await expect(companyCall).toHaveText(companyContText)
+    await expect(nameCall).toHaveText(nameContText)
   })
+
 })
