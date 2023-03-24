da7fcf5ebcbe45d1ba3d524b2a872c2ea4e0cd4e
lino <lino.valverde@a2odev.com>
2023-03-24 11:34:51 -0400

    feature/RI-878

--- test/moduleSiteAdmin/PotentiaDuplicateNotFound.spec.ts
+++ test/moduleSiteAdmin/PotentiaDuplicateNotFound.spec.ts
@@ -9,27 +9,24 @@ const env = process.env
 
 describe('Potential Duplicates Test', () => {
     const url = env.PFR_URL + 'auth/login'
-    it('Potential Duplicate', async () => {
+    const LeadsAndDonors = env.PFR_URL +'donors'
+    it('Potential Duplicate - Not Found Duplicate ', async () => {     
       await browser.url(url)
       await $('#username').setValue(env.SITEADMIN_USERNAME)
       await $('#password').setValue(env.SITEADMIN_PASSWORD)
       await $('input[type="submit"]').click()
-      
+      await browser.url(LeadsAndDonors)
       await browser.pause(3000)
-      const Site = await $('//*[@id="navbar"]/ul[2]/li[4]/a')
-      const LeadsDonors = await $('//*[@id="navbar"]/ul[2]/li[4]/ul/li[5]/a')
+      
       const View = await $('//*[@id="donors"]/tbody/tr[1]/td[4]/button')
-      const ClickHere = await $('/html/body/div/div/div[2]/div[3]/div[5]/div/div[2]/p/a')
-      
-      Site.click()
-      await browser.pause(3000)
-      LeadsDonors.click()
-      await browser.pause(3000)
+      const ClickHere = await $('=Click here')
       View.click()
       await browser.pause(3000)
       expect(await $('h1=Donor Details')).toBeExisting()
-
-      ClickHere.click()
       
+      ClickHere.click()
+      await browser.pause(3000)
+      await expect(ClickHere).toBeExisting()
+      await expect($('/html/body/div/div/div[2]/div[3]/div[5]/div/div[2]/p')).toBeExisting()
     })
   })
