4c0d919de69c130bb76deba1369934903d56f6ae
Maricruzli <maricruz.quispe@a2odev.com>
2023-03-30 15:03:08 -0400

    feature/RI-746 Button duplicate

--- test/moduleCallerAdmin/buttonsfuncionality.spec.ts
+++ test/moduleCallerAdmin/buttonsfuncionality.spec.ts
@@ -17,11 +17,19 @@ const url = env.PFR_URL + 'auth/login'
       await browser.pause(6000)
     })
     it('Call buttons funcionality', async () => {
-      const btno_answer = await $('//*[@id="callActions"]/button[2]')
+      const btno_answer = await $$('[type="button"]')[3]
       const btcontacout = await $('//*[@id="callActions"]/button[3]')
       const btdisconnected = await $('//*[@id="callActions"]/button[4]')
       const btrejected = await $('//*[@id="callActions"]/button[5]')
       const btnocall = await $('//*[@id="callActions"]/button[6]')
+      //const button = await $("[id='takePledgeButton']")
+     // const amount = await $("[placeholder='Amount']")
+      //const in_Kind_Pledge = await $("[value='1']")
+      //const method = $("[value='Mail']")
+      //const save_pledge = await $$('button[type="submit"]')[2]
+      //const confirm = await $('aria/Submit Pledge')
+      //await button.click()
+     // await amount.waitForDisplayed()
       await btno_answer.click()
       await browser.pause(10000)
       await btcontacout.click()
