ec06a1489ba2925c106db2ef6fce36dad3327390
Maricruzli <maricruz.quispe@a2odev.com>
2023-03-30 16:18:13 -0400

    feature/RI-745unneeded space removed

--- test/moduleCallerAdmin/savePledge.spec.ts
+++ test/moduleCallerAdmin/savePledge.spec.ts
@@ -8,33 +8,34 @@ const env = process.env
 describe('Logging in to the system', () => { 
 const url = env.PFR_URL + 'auth/login'
 
-    //Login with swalter user to the system
-    it('login', async () => {
-      await browser.url(url)
-      await browser.maximizeWindow()
-      await $('#username').setValue(env.CALLER_USERNAME)
-      await $('#password').setValue(env.CALLER_PASSWORD)
-      await $('input[type="submit"]').click()    
-      await browser.pause(6000)
-    })
-
-    //Method of payment by "Drop off"
-    it('Call Save the Pledge', async () => {
-      const button = await $$('[type="button"]')[3]
-      const amount = await $("[placeholder='Amount']")
-      const in_Kind_Pledge = await $("[value='1']")
-      const method = await $("[value='Drop off']")
-      const savepledge = await $$('button[type="submit"]')[2]
-      const buttonconfirm = await $('aria/Submit Pledge')
-      await button.click()
-      await amount.waitForDisplayed()
-      await amount.setValue(env.CALLER_AMOUNT)
-      await in_Kind_Pledge.waitForDisplayed()
-      await in_Kind_Pledge.click()
-      await method.waitForDisplayed()
-      await method.click()
-      await savepledge.click()
-      await buttonconfirm.waitForDisplayed()
-      await buttonconfirm.click()
-    })
+  //Login with Caller user to the system
+  it('login', async () => {
+    await browser.url(url)
+    await browser.maximizeWindow()
+    await $('#username').setValue(env.CALLER_USERNAME)
+    await $('#password').setValue(env.CALLER_PASSWORD)
+    await $('input[type="submit"]').click()    
+    await browser.pause(6000)
   })
+
+  //Method of payment by "Drop off"
+  it('Call Save the Pledge', async () => {
+    const button = await $$('[type="button"]')[3]
+    const amount = await $("[placeholder='Amount']")
+    const in_Kind_Pledge = await $("[value='1']")
+    const method = await $("[value='Drop off']")
+    const savepledge = await $$('button[type="submit"]')[2]
+    const buttonconfirm = await $('aria/Submit Pledge')
+    await button.click()
+    await amount.waitForDisplayed()
+    await amount.setValue(env.CALLER_AMOUNT)
+    await in_Kind_Pledge.waitForDisplayed()
+    await in_Kind_Pledge.click()
+    await method.waitForDisplayed()
+    await method.click()
+    await savepledge.click()
+    await buttonconfirm.waitForDisplayed()
+    await buttonconfirm.click()
+  })
+
+})
