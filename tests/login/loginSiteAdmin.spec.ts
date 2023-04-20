ab8cb5d55e66bc3b8e2b252f88e57eb14aa08163
Giselle026 <giselle.castro@a2odev.com>
2023-04-20 19:31:56 -0400

    feature/RI-982

--- tests/login/loginSiteAdmin.spec.ts
+++ tests/login/loginSiteAdmin.spec.ts
@@ -1,24 +1,23 @@
+import { selectors } from '../dictionaries/selectors.ts'
 import * as dotenv from 'dotenv'
 
 const envFound = dotenv.config();
 if (envFound.error) {
   throw new Error("Couldn't find .env file");
 }
-
 const env = process.env
-
 describe('Login like a siteadmin', () => {
-    const url = env.PFR_URL + 'auth/login'
-    it('should have a complete UI', async () => {
-      await browser.url(url)
-      
-      await expect($('#username')).toBeExisting()
-      await expect($('#password')).toBeExisting()
-    })
-    it('login', async () => {
-      await browser.url(url)
-      await $('#username').setValue(env.SITEADMIN_USERNAME)
-      await $('#password').setValue(env.SITEADMIN_PASSWORD)
-      await $('input[type="submit"]').click()    
-    })
+  const url = env.PFR_URL + 'auth/login'
+  it('should have a complete UI', async () => {
+    await browser.url(url)
+    await expect($(selectors.userName)).toBeExisting()
+    await expect($(selectors.password)).toBeExisting()
   })
+  it('login', async () => {
+    await browser.url(url)
+
+    await $(selectors.userName).setValue(env.SITEADMIN_USERNAME)
+    await $(selectors.password).setValue(env.SITEADMIN_PASSWORD)
+    await $(selectors.bntLogin).click()
+  })
+})
