6bc6aa27c589fbedb5f9ae149c2cb84e0fe1230b
Giselle026 <giselle.castro@a2odev.com>
2023-04-20 20:18:59 -0400

    feature/RI-982

--- tests/dictionaries/selectors.ts
+++ tests/dictionaries/selectors.ts
@@ -1,9 +1,9 @@
 // Exporta el diccionario de selectores como un objeto con una propiedad "selectors"
 export const selectors = {
-
-    userName           : '#username',
-    password           : '#password',
-    bntLogin           : 'input[type="submit"]'
+ //login
+    userName           :'#username',
+    password           :'#password',
+    bntLogin           :'input[type="submit"]'
 
 };
 // Exporta el objeto completo, si lo desea
