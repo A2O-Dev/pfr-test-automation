fd5ffa5dc8f76a9d52d4af34e4ec6cfd3b66ceee
Maricruzli <maricruz.quispe@a2odev.com>
2023-04-25 00:55:10 -0400

    Automation test-verify payment for admin

---
+++ tests/verifypayment/verifypaymentadmin.spec.ts
@@ -0,0 +1,28 @@
+import { login } from '../../dictionaries/selectors/index.ts'
+import * as dotenv from 'dotenv'
+
+const envFound = dotenv.config();
+const env = process.env
+
+describe('Login like a siteadmin', () => {
+  const url = env.PFR_URL + 'auth/login'
+  //Login as user Admin
+  it('login', async () => {
+    await browser.url(url)
+    await browser.maximizeWindow()
+    const name = await $(login.userName)
+    const password = await $(login.password)
+
+    await $(name).setValue(env.SITEADMIN_USERNAME)
+    await $(password).setValue(env.SITEADMIN_PASSWORD)
+    await $(login.btnLogin).click()
+
+    await expect($(name)).toBeExisting()
+    await expect($(password)).toBeExisting()
+  })
+  //verifying payment by Admin
+  it('Verify payment', async () => {
+    (await $("a[href='/confirmations']")).click()
+    
+  })
+})
