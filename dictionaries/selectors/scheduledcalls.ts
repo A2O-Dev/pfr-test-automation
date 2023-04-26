c3cd65160c4b89536b44a74e254c15c18c63108e
Maricruzli <maricruz.quispe@a2odev.com>
2023-04-26 10:46:30 -0400

    Adding modifications according to review comments

--- dictionaries/selectors/scheduledcalls.ts
+++ dictionaries/selectors/scheduledcalls.ts
@@ -1,8 +1,8 @@
 export const scheduled = {
     companyContactTable :'//table/tbody[1]/tr/td[1]',
     nameContactTable    :'//table/tbody[1]/tr/td[2]',
-    btnCall             :'//table/tbody[1]/tr/td[9]/a'
-
+    btnCall             :'//table/tbody[1]/tr/td[9]/a',
+    menuConfirm         :"a[href='/confirmations']"
 
 
 }
--- tests/verifypayment/verifypaymentadmin.spec.ts
+++ tests/verifypayment/verifypaymentadmin.spec.ts
@@ -1,4 +1,4 @@
-import { login } from '../../dictionaries/selectors/index.ts'
+import { login, scheduled } from '../../dictionaries/selectors/index.ts'
 import * as dotenv from 'dotenv'
 
 const envFound = dotenv.config();
@@ -6,16 +6,13 @@ const env = process.env
 
 describe('Login like a siteadmin', () => {
   const url = env.PFR_URL + 'auth/login'
-  //verifying payment by Admin
   it('verifying payment', async () => {
     await browser.url(url)
     await browser.maximizeWindow()
-    const name = await $(login.userName)
-    const password = await $(login.password)
-    await $(name).setValue(env.SITEADMIN_USERNAME)
-    await $(password).setValue(env.SITEADMIN_PASSWORD)
+    await $(login.userName).setValue(env.SITEADMIN_USERNAME)
+    await $(login.password).setValue(env.SITEADMIN_PASSWORD)
     await $(login.btnLogin).click()
-    await $("a[href='/confirmations']").click()
+    await $(scheduled.menuConfirm).click()
   })
   
 })
