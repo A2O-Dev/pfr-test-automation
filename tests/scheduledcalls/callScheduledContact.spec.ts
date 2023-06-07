694ece3c0dd121f6db10659523d4b70abcd9e2e1
lino <lino.valverde@a2odev.com>
2023-06-07 11:46:01 -0400

    feature/RI-936

--- tests/scheduledcalls/callScheduledContact.spec.ts
+++ tests/scheduledcalls/callScheduledContact.spec.ts
@@ -2,27 +2,31 @@ import { scheduled, login  } from '../../dictionaries/selectors/index.ts'
 import { urls, credentials } from '../../dictionaries/config/index.ts'
 
 describe('Call the scheduled contact', () => {
+ 
   before('Login', async () => {
     await browser.url(urls.login)
     await $(login.userName).setValue(credentials.caller.username)
     await $(login.password).setValue(credentials.caller.password)
-    await $(login.btnLogin).click()  
-  })
+    await $(login.btnLogin).click()
+    
+})
+
 
   it('Call a scheduled contact', async () => {
-
+    await browser.url(urls.callerFindLead) 
+    await new Promise(resolve => setTimeout(resolve, 3000))
     const scheduledCallsButton = await $(scheduled.scheduledCallButton)
     const companyContactTable = await $(scheduled.companyContactTable)
     const nameContactTable = await $(scheduled.nameContactTable)
     const buttonCall = await $(scheduled.buttonCall)
     const companyContText = await companyContactTable.getText()
     const nameContText = await nameContactTable.getText()
-    
+    await scheduledCallsButton.waitForClickable()
     await scheduledCallsButton.click()
     await expect($(scheduled.scheduledCallTitle)).toBeExisting()
     await expect($(scheduled.scheduledCallerTable)).toBeExisting()
-    
-    buttonCall.click();
+    await buttonCall.waitForClickable()
+    await buttonCall.click()
 
     await browser.waitUntil(async () => {
       return (await $('.nameAndCompany .company').isDisplayed())
